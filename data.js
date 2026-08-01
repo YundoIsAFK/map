bd76075c5f615636fe7892ca4a10068f3b5ffbdc
YundoIsAFK <pinky.bi.ch@gmail.com>
2026-08-01 12:28:34 +0300

    Refactor tile creation functions and presets

--- data.js
+++ data.js
@@ -72,7 +72,7 @@ const TILES = {
         src: './img/standard/grass/Grassland.png',
         buildable: true,
         passable: true,
-        cost: 25,
+        cost: 22,
         points: 1,
         defense_modifier: 0.8,
         attack_modifier: 1.0
@@ -83,7 +83,7 @@ const TILES = {
         src: './img/standard/grass/Green_Hills.png',
         buildable: true,
         passable: true,
-        cost: 33,
+        cost: 29,
         points: 1,
         defense_modifier: 1.1,
         attack_modifier: 0.9
@@ -101,10 +101,10 @@ const TILES = {
         src: './img/standard/grass/Rocky_Hills.png',
         buildable: false,
         passable: true,
-        cost: 38,
+        cost: 34,
         points: 1,
-        defense_modifier: 0.7,
-        attack_modifier: 1.5
+        defense_modifier: 1.5,
+        attack_modifier: 0.7
       }),
       makeTile({
         name: 'Forest',
@@ -112,7 +112,7 @@ const TILES = {
         src: './img/standard/grass/Forest.png',
         buildable: true,
         passable: true,
-        cost: 45,
+        cost: 40,
         points: 1,
         defense_modifier: 1.2,
         attack_modifier: 0.8
@@ -123,7 +123,7 @@ const TILES = {
         src: './img/standard/grass/Swamp.png',
         buildable: true,
         passable: true,
-        cost: 50,
+        cost: 55,
         points: 1,
         defense_modifier: 1.0,
         attack_modifier: 1.0
@@ -154,7 +154,7 @@ const TILES = {
         src: './img/standard/sand/Green_Hills.png',
         buildable: true,
         passable: true,
-        cost: 33,
+        cost: 29,
         points: 1,
         defense_modifier: 1.1,
         attack_modifier: 0.9
@@ -183,7 +183,7 @@ const TILES = {
         src: './img/standard/sand/Forest.png',
         buildable: true,
         passable: true,
-        cost: 45,
+        cost: 40,
         points: 1,
         defense_modifier: 1.2,
         attack_modifier: 0.8
@@ -194,7 +194,7 @@ const TILES = {
         src: './img/standard/sand/Swamp.png',
         buildable: true,
         passable: true,
-        cost: 50,
+        cost: 55,
         points: 1,
         defense_modifier: 1.0,
         attack_modifier: 1.0
@@ -462,8 +462,8 @@ const TILES = {
         src: './img/special/grass/Yggdrasil.png',
         buildable: false,
         passable: true,
-        cost: 20,
-        points: 35,
+        cost: 22,
+        points: 25,
         defense_modifier: 1.2,
         attack_modifier: 0.8
       })
